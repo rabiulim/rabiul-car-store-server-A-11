1881a6c2ee2a024e8095da99ce28d96aa32fb0c3
rabiulim <rabiulim2022@gmail.com>
2022-05-13 19:24:44 +0600

    delete from db done

--- index.js
+++ index.js
@@ -45,12 +45,12 @@ async function run() {
         //     res.send(result);
         // });
 
-        // app.delete('/cars/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const result = await carCollection.deleteOne(query);
-        //     res.send(result);
-        // });
+        app.delete('/car/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await carCollection.deleteOne(query);
+            res.send(result);
+        });
     }
 
     finally {
